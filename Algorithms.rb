2dfef826197fc2fd9247db3006beaa878705a39f
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2020-04-20 11:17:51 +0200

    Update Algorithms.rb

--- Algorithms.rb
+++ Algorithms.rb
@@ -890,3 +890,63 @@ arr_2 = [
 
 print two_d_translate(arr_2) # => [ 'red', 'blue', 'blue', 'blue', 'blue' ]
 puts
+
+
+
+32.Get Double
+#Write a method get_double_age that takes in a hash and returns twice the "age" value of the hash.
+
+
+def get_double_age(hash)
+
+return hash["age"]*2
+  
+end
+
+puts get_double_age({"name"=>"App Academy", "age"=>5}) # => 10
+puts get_double_age({"name"=>"Ruby", "age"=>23})       # => 46
+
+
+
+33.Get Full Name
+#Write a method get_full_name that takes in a hash containing a first, last, and title. 
+#The method should return a string representing the hash's full name.
+
+
+def get_full_name(hash)
+  
+return puts hash["first"]+" "+hash["last"]+ ", "+hash["title"]
+   
+end
+
+hash1 = {"first"=>"Michael", "last"=>"Jordan", "title"=> "GOAT"}
+puts get_full_name(hash1) # => "Michael Jordan, the GOAT"
+
+hash2 = {"first"=>"Fido", "last"=>"McDog", "title"=> "Loyal"}
+puts get_full_name(hash2) # => "Fido McDog, the Loyal"
+
+
+34.Word Lengths
+#Write a method word_lengths that takes in a sentence string and returns a hash where every key is a word of the sentence,
+# and its' corresponding value is the length of that word.
+
+
+def word_lengths(sentence)
+  strArr = sentence.split(" ")
+
+  noviObjekat = Hash.new(0)
+  strArr.each do |elem|
+    noviObjekat[elem]+=elem.length
+    
+  end
+  
+  return noviObjekat
+  
+end
+
+puts word_lengths("this is fun") #=> {"this"=>4, "is"=>2, "fun"=>3}
+puts word_lengths("When in doubt, leave it out") #=> {"When"=>4, "in"=>2, "doubt,"=>6, "leave"=>5, "it"=>2, "out"=>3}
+
+
+35.
+
