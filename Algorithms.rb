d51c487d665ec41b54e7ebddfc7b8053e9fead5f
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2020-07-31 10:07:35 +0200

    Update Algorithms.rb

--- Algorithms.rb
+++ Algorithms.rb
@@ -1464,3 +1464,36 @@ print triple_sequence(2, 4) # => [2, 6, 18, 54]
 puts
 print triple_sequence(4, 5) # => [4, 12, 36, 108, 324]
 puts
+
+
+
+54.Summation Sequence
+#A number's summation is the sum of all positive numbers less than or equal to the number. 
+#For example: the summation of 3 is 6 because 1 + 2 + 3 = 6, the summation of 6 is 21 because 1 + 2 + 3 + 4 + 5 + 6 = 21.
+#Write a method summation_sequence that takes in a two numbers: start and length. The method should return an array containing length total elements. 
+#The first number of the sequence should be the start number. At any point, to generate the next element of the sequence we take the summation of the previous element.
+#You can assume that length is not zero.
+
+
+def summation_sequence(start, length)
+  arr = [start]
+  
+ (length-1).times do 
+   s = arr[-1]
+    sum = summing(s)
+    arr << sum
+ end
+ return arr
+end
+
+def summing(s)
+    v = 0
+    (1..s).each do |num|
+      v += num
+    end
+    return v
+end
+
+
+print summation_sequence(3, 4) # => [3, 6, 21, 231]
+print summation_sequence(5, 3) # => [5, 15, 120]
