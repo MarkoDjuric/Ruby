6dc3042584a7b3f495bd17a1457ec8129d87002d
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2020-10-02 23:54:48 +0200

    Update Algorithms.rb

--- Algorithms.rb
+++ Algorithms.rb
@@ -2024,4 +2024,40 @@ print array_translate(["red", 3, "blue", 1]); # => "redredredblue"
 puts
 
 
+70.Element Replace
+#Write a method element_replace that takes in an array and a hash. 
+#The method should return a new array where elements of the original array are replaced with their corresponding values in the hash.
+
+
+def element_replace(arr, hash)
+ new_arr=[]
+  temp_arr =[]
+  arr.each_with_index do |el, i|
+    hash.each do  |key, val|
+      if el == key 
+        new_arr << val 
+       elsif !hash.has_key?(el)
+        if !new_arr.include?(el)
+       new_arr << el
+        end
+      end
+       
+      end
+    end
+  
+     
+  return new_arr
+  
+end
+
+arr1 = ["LeBron James", "Lionel Messi", "Serena Williams"]
+hash1 = {"Serena Williams"=>"tennis", "LeBron James"=>"basketball"}
+print element_replace(arr1, hash1) # => ["basketball", "Lionel Messi", "tennis"]
+puts
+
+arr2 = ["dog", "cat", "mouse"]
+hash2 = {"dog"=>"bork", "cat"=>"meow", "duck"=>"quack"}
+print element_replace(arr2, hash2) # => ["bork", "meow", "mouse"]
+puts
+
 
